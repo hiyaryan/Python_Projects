b49f56e0aaa573d9fd72d63509e4866ff05c43e5
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-08-14 16:35:07 -0700

    Add rock, paper, scissors game.

--- Rock_Paper_Scissors/main.py
+++ Rock_Paper_Scissors/main.py
@@ -1,2 +1,24 @@
 # A casual game of Rock-Paper-Scissors
 
+import random
+
+def play():
+    user = input("'r' for rock, 'p' for paper, 's' for scissors: ")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'tie'
+
+    if is_win(user, computer):
+        return 'You win!'
+
+    return 'You lost!'
+
+
+def is_win(player, opponent):
+    # return true if player wins
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+
+print(play())
