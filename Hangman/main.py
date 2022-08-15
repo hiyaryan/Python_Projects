2b1b8dbcad1187aff073f6588a8c4905c5ca31d9
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-08-14 17:13:07 -0700

    Add game with unlimited guessing.

--- Hangman/main.py
+++ Hangman/main.py
@@ -2,3 +2,53 @@
 
 import random
 from words import words
+import string
+
+
+def get_valid_word():
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word()
+    # print(f'word: {word}')
+
+    word_letters = set(word)  # letters in the word
+    # print(f'word letters: {word_letters}')
+
+    alphabet = set(string.ascii_uppercase)
+    # print(f'alphabet: {alphabet}')
+
+    used_letters = set()  # what the user has guessed
+    # print(f'used letters: {used_letters}')
+
+    # getting user input
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print(f'You have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+
+        else:
+            print('Invalid character. Please try again')
+
+
+hangman()
+
