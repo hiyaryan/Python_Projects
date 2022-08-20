f17f383a4bce0f1257a84fd784bb35a9d5deb658
Ryan Meneses <meneses.ryan.j@gmail.com>
2022-08-19 21:38:59 -0700

    Fix typo and spacing.

--- Tic_Tac_Toe/player.py
+++ Tic_Tac_Toe/player.py
@@ -1,12 +1,13 @@
 import math
 import random
 
+
 class Player:
     def __init__(self, letter):
         # letter is c or o
         self.letter = letter
 
-    # watn all players to get their next move
+    # want all players to get their next move
     def get_move(self, game):
         pass
 
